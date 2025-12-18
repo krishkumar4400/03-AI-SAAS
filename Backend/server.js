fc5e061205668a219bf1b9cf797d629adf0fe3de
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-12-18 10:39:51 +0530

    backend server

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import { clerkMiddleware, requireAuth} from "@clerk/express";
-import aiRouter from './Routes/aiRoutes.js'
-import connectCloudinary from './Config/cloudinary.js';
-import { auth } from './Middlewares/auth.js';
-import userRouter from './Routes/userRoutes.js';
-import connectMongoDB from './Config/mongoDB.js';
-import blogRouter from './Routes/postBlog.js';
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+import aiRouter from "./Routes/aiRoutes.js";
+import connectCloudinary from "./Config/cloudinary.js";
+import { auth } from "./Middlewares/auth.js";
+import userRouter from "./Routes/userRoutes.js";
+import connectMongoDB from "./Config/mongoDB.js";
+import blogRouter from "./Routes/postBlog.js";
 
 const app = express();
 
@@ -24,8 +24,8 @@ app.use(
   })
 );
 
-app.get('/', (req,res) => {
-    res.send("hello express"); 
+app.get("/", (req, res) => {
+  res.send("hello express");
 });
 
 app.use("/api/post", blogRouter);
@@ -35,10 +35,10 @@ app.use(clerkMiddleware());
 app.use(requireAuth());
 
 app.use("/api/ai", auth, aiRouter);
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
